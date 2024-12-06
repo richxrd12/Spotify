8abf07bd0672235e03b8bab5df018206dc37fc05
richxrd7 <richardquintanapadron@gmail.com>
2024-12-06 11:31:07 +0000

    Pusheando...

--- PrimerosPasosSpotifyAlike/Model/User.cs
+++ PrimerosPasosSpotifyAlike/Model/User.cs
@@ -12,5 +12,6 @@ namespace PrimerosPasosSpotifyAlike.Model
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
     }
 }
